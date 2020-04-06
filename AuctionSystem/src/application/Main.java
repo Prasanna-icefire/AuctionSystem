55e0d00b937438e7e114eaec123f9400b6ca60e7
icefire <icefire@kali.com>
2020-04-06 20:36:54 +0530

    understanding fx

--- AuctionSystem/src/application/Main.java
+++ AuctionSystem/src/application/Main.java
@@ -1,15 +1,25 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+
 
 
 public class Main extends Application {
+	
+	Button item;
 	@Override
 	public void start(Stage primaryStage) {
+		primaryStage.setTitle("Auction System");
+		item = new Button("Item");
+		StackPane layout = new StackPane();
+		layout.getChildren().add(item);
 		
 	}
 	
