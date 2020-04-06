a547f5ac90a50c9c5e48e6dcaa06b5484d1d5378
icefire <icefire@kali.com>
2020-04-07 01:25:25 +0530

    Bugged with Errors!

---
+++ AuctionSystem/src/application/AdminLoginController.java
@@ -0,0 +1,25 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+
+public class AdminLoginController implements Initializable{
+	
+	@FXML
+	private AnchorPane pane;
+	void function()
+	{
+		
+	}
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
--- AuctionSystem/src/application/MainController.java
+++ AuctionSystem/src/application/MainController.java
@@ -1,18 +1,40 @@
 package application;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
-public class MainController {
+public class MainController implements Initializable {
 	
 	@FXML
 	private Label msg;
-	
-	public void adminlogin(ActionEvent event)
+	private AnchorPane rootpane;
+	public void adminlogin(ActionEvent event) throws Exception
 	{
 		//msg.setText("yolo");
+		Parent pane;
 		System.out.println("To enter starting bidding prices");
+		try
+		{
+			pane = (AnchorPane) FXMLLoader.load(getClass().getResource("Admin_login.fxml"));
+			Scene scene = new Scene(pane);
+			Stage curStage = (Stage)rootpane.getScene().getWindow();
+			curStage.setScene(scene);
+		}catch(IOException e)
+		{
+			
+		}
+		
 	}
 	
 	@FXML
@@ -27,6 +49,12 @@ public class MainController {
 	{
 		//msg.setText("yolo");
 		System.out.println("Sold");
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
