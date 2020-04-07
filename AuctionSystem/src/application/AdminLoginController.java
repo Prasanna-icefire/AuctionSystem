7cc8252ad502e46fff8c679f9b5f89e736b03296
icefire <icefire@kali.com>
2020-04-07 13:03:03 +0530

    Got the password validation

--- AuctionSystem/src/application/AdminLoginController.java
+++ AuctionSystem/src/application/AdminLoginController.java
@@ -9,17 +9,33 @@ import javax.swing.Action;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 
 public class AdminLoginController implements Initializable{
 	
 	@FXML
+	private Label msg;
+	@FXML
 	private PasswordField pass;
+
 	public void getpassword(ActionEvent event) throws IOException 
 	{
 		String password = pass.getText();
 		System.out.println(password);
+		if(password.equals("password"))
+			{
+			System.out.println("Correct");
+			msg.setText("valid");
+			}
+			
+		else
+		{
+			System.out.println("Incorrect");
+			msg.setText("Invalid!");
+		}
 	}
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
