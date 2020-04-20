cef21285852a874ca179473e8279edba3adf417b
icefire <icefire@kali.com>
2020-04-21 04:38:28 +0530

    Successfully started mysqlthrough cmd

--- AuctionSystem/src/application/Main.java
+++ AuctionSystem/src/application/Main.java
@@ -18,11 +18,29 @@ public class Main extends Application {
 	public static void main(String[] args) {
 		launch(args);
 	}
-	
+	public void runTask()
+	{
+		String command = "echo 12345678 | sudo -S -k service mysql start";
+
+
+		try {
+			Process process = Runtime.getRuntime().exec(command);
+
+
+			System.out.println("Executing cmd");
+			Thread.sleep(5000);
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+		
+	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception
 	{ 
+		runTask();
 		Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
 		Scene scene  = new Scene(root);
 		primaryStage.setScene(scene);
@@ -30,4 +48,5 @@ public class Main extends Application {
 		primaryStage.show();
 		
 	}
+
 }
